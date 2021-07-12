4d2b4c1d294fb6b7ed88c5ecf99c8f2438a572e8
Haky86 <hakamelassouad@gmail.com>
2021-07-13 00:00:49 +0200

    sm6150-common: Initial commit
    Change-Id: I4536ba15f62da111a372eb8af2ef618f87474896

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/samsung/sm6150-common
+
+include vendor/samsung/sm6150-common/BoardConfigVendor.mk
---
+++ sm6150.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, vendor/samsung/sm6150-common/sm6150-common-vendor.mk)
