01d2fe75a5c166cf23b451fa97bf328475f4f557
Haky86 <hakamelassouad@gmail.com>
2021-07-13 00:30:41 +0200

    sm6150-common: Setup arch
    Change-Id: I239e2c8a7aa9fdb3bdfde377341bdc2fea561e9d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,6 +4,24 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BOARD_VENDOR := samsung
+
 COMMON_PATH := device/samsung/sm6150-common
+
+# 64 Bit
+TARGET_USES_64_BIT_BINDER := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
 
 include vendor/samsung/sm6150-common/BoardConfigVendor.mk
