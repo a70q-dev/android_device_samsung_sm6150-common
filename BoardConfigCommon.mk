448adde88fdd9dd363a3f298a201283c50660a14
Haky86 <hakamelassouad@gmail.com>
2021-07-13 00:36:59 +0200

    sm6150-common: Setup kernel-inline
    * Make BOARD_HEADER_VERSION & BOARD_NAME device-specific due to different shipped version.
    
    Change-Id: I449f4c0a4bfc04ed3be1461b7bd578c8aabfcf82

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,4 +29,21 @@ TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 TARGET_USES_UEFI := true
 
+# Kernel
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 firmware_class.path=/vendor/firmware_mnt/image nokaslr printk.devkmsg=on
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_KERNEL_OFFSET      := 0x00008000
+BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_SECOND_OFFSET      := 0x00f00000
+BOARD_MKBOOTIMG_ARGS     := --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) --second_offset $(BOARD_SECOND_OFFSET)
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+TARGET_KERNEL_SOURCE := kernel/samsung/sm6150
+
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+TARGET_KERNEL_ARCH := arm64 
+TARGET_KERNEL_CLANG_COMPILE := true
+
 include vendor/samsung/sm6150-common/BoardConfigVendor.mk
